54138fabd2849e9a1be9fe61ccbadd3e368913b4
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2024-01-29 11:04:24 +0500

    Add files via upload

---
+++ Input_By_Scanner/secondLecture.java
@@ -0,0 +1,42 @@
+public class secondLecture {
+
+    public static void main(String[] args) {
+        //Use The Single Quotes not Possible
+        /*System.out.print("Sufyan Ahmad");
+        */
+
+        // Print The Single Line Futher Commands Use The Only Print
+        /* System.out.print("Sufyan Ahmad");
+        System.out.print("My Name is Sufyan Ahmad ");
+        System.out.print("Sufyan Ahmad");
+        System.out.print("My Name is Sufyan Ahmad ");
+    */
+        // Print The next  Line Futher Commands Use The Only Println
+      /*
+        System.out.println("Sufyan Ahmad");
+        System.out.println("My Name is Sufyan Ahmad ");
+        System.out.println("Sufyan Ahmad");
+        System.out.print("My Name is Sufyan Ahmad ");
+      */
+
+        // Print The Next  Line Futher Commands Use The Only \n
+
+
+      /*
+
+
+        System.out.print("Sufyan Ahmad \n");
+        System.out.print("My Name is Sufyan Ahmad \n");
+        System.out.print("Sufyan Ahmad \n");
+        System.out.print("My Name is Sufyan Ahmad ");
+    */
+        // Print The Next  Line Futher Commands Use The Only \n
+
+        /*
+        System.out.print("My Name is Sufyan Ahmad \n My Age Age Is 23\n I Am Learn Java Programming Language");
+
+ */
+    }
+
+
+}
